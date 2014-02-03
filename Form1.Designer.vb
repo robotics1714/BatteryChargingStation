79a9dba084ba1a7c63e8d1538b35564a1c1d0514
MORE Robotics Team 1714 <robotics1714@gmail.com>
2014-02-03 17:38:19 -0600

    Added calculations to find the current the batteries are drawing
    I added some calculations that determines how much current the batteries
    are drawing. The calculation for the percent done needs to be fixed. I
    also added an icon for the program.
    
    Bijan

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,6 +22,8 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Charger1 = New System.Windows.Forms.ProgressBar()
         Me.Charger2 = New System.Windows.Forms.ProgressBar()
         Me.Charger3 = New System.Windows.Forms.ProgressBar()
@@ -30,53 +32,72 @@ Partial Class Form1
         Me.Charger6 = New System.Windows.Forms.ProgressBar()
         Me.TurnOn = New System.Windows.Forms.Button()
         Me.TurnOff = New System.Windows.Forms.Button()
+        Me.WaitTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.CurrentLbl1 = New System.Windows.Forms.Label()
+        Me.CurrentLbl2 = New System.Windows.Forms.Label()
+        Me.CurrentLbl3 = New System.Windows.Forms.Label()
+        Me.CurrentLbl4 = New System.Windows.Forms.Label()
+        Me.CurrentLbl5 = New System.Windows.Forms.Label()
+        Me.CurrentLbl6 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Charger1
         '
-        Me.Charger1.Location = New System.Drawing.Point(12, 12)
+        Me.Charger1.Location = New System.Drawing.Point(13, 29)
         Me.Charger1.Name = "Charger1"
         Me.Charger1.Size = New System.Drawing.Size(164, 23)
         Me.Charger1.TabIndex = 0
+        Me.Charger1.Value = 100
         '
         'Charger2
         '
-        Me.Charger2.Location = New System.Drawing.Point(13, 42)
+        Me.Charger2.Location = New System.Drawing.Point(12, 75)
         Me.Charger2.Name = "Charger2"
         Me.Charger2.Size = New System.Drawing.Size(163, 23)
         Me.Charger2.TabIndex = 1
+        Me.Charger2.Value = 100
         '
         'Charger3
         '
-        Me.Charger3.Location = New System.Drawing.Point(13, 72)
+        Me.Charger3.Location = New System.Drawing.Point(12, 121)
         Me.Charger3.Name = "Charger3"
         Me.Charger3.Size = New System.Drawing.Size(163, 23)
         Me.Charger3.TabIndex = 2
+        Me.Charger3.Value = 100
         '
         'Charger4
         '
-        Me.Charger4.Location = New System.Drawing.Point(13, 102)
+        Me.Charger4.Location = New System.Drawing.Point(12, 167)
         Me.Charger4.Name = "Charger4"
         Me.Charger4.Size = New System.Drawing.Size(163, 23)
         Me.Charger4.TabIndex = 3
+        Me.Charger4.Value = 100
         '
         'Charger5
         '
-        Me.Charger5.Location = New System.Drawing.Point(13, 132)
+        Me.Charger5.Location = New System.Drawing.Point(13, 213)
         Me.Charger5.Name = "Charger5"
         Me.Charger5.Size = New System.Drawing.Size(163, 23)
         Me.Charger5.TabIndex = 4
+        Me.Charger5.Value = 100
         '
         'Charger6
         '
-        Me.Charger6.Location = New System.Drawing.Point(13, 162)
+        Me.Charger6.Location = New System.Drawing.Point(12, 259)
         Me.Charger6.Name = "Charger6"
         Me.Charger6.Size = New System.Drawing.Size(163, 23)
         Me.Charger6.TabIndex = 5
+        Me.Charger6.Value = 100
         '
         'TurnOn
         '
-        Me.TurnOn.Location = New System.Drawing.Point(260, 11)
+        Me.TurnOn.Location = New System.Drawing.Point(321, 121)
         Me.TurnOn.Name = "TurnOn"
         Me.TurnOn.Size = New System.Drawing.Size(75, 23)
         Me.TurnOn.TabIndex = 6
@@ -85,18 +106,143 @@ Partial Class Form1
         '
         'TurnOff
         '
-        Me.TurnOff.Location = New System.Drawing.Point(369, 11)
+        Me.TurnOff.Location = New System.Drawing.Point(321, 145)
         Me.TurnOff.Name = "TurnOff"
         Me.TurnOff.Size = New System.Drawing.Size(75, 23)
         Me.TurnOff.TabIndex = 7
         Me.TurnOff.Text = "Turn Off"
         Me.TurnOff.UseVisualStyleBackColor = True
         '
+        'WaitTimer
+        '
+        Me.WaitTimer.Enabled = True
+        Me.WaitTimer.Interval = 250
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Charger 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Charger 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 105)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Charger 3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 151)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Charger 4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 197)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Charger 5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(13, 243)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(53, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Charger 6"
+        '
+        'CurrentLbl1
+        '
+        Me.CurrentLbl1.AutoSize = True
+        Me.CurrentLbl1.Location = New System.Drawing.Point(198, 39)
+        Me.CurrentLbl1.Name = "CurrentLbl1"
+        Me.CurrentLbl1.Size = New System.Drawing.Size(23, 13)
+        Me.CurrentLbl1.TabIndex = 14
+        Me.CurrentLbl1.Text = "0 A"
+        '
+        'CurrentLbl2
+        '
+        Me.CurrentLbl2.AutoSize = True
+        Me.CurrentLbl2.Location = New System.Drawing.Point(198, 85)
+        Me.CurrentLbl2.Name = "CurrentLbl2"
+        Me.CurrentLbl2.Size = New System.Drawing.Size(23, 13)
+        Me.CurrentLbl2.TabIndex = 15
+        Me.CurrentLbl2.Text = "0 A"
+        '
+        'CurrentLbl3
+        '
+        Me.CurrentLbl3.AutoSize = True
+        Me.CurrentLbl3.Location = New System.Drawing.Point(198, 131)
+        Me.CurrentLbl3.Name = "CurrentLbl3"
+        Me.CurrentLbl3.Size = New System.Drawing.Size(23, 13)
+        Me.CurrentLbl3.TabIndex = 16
+        Me.CurrentLbl3.Text = "0 A"
+        '
+        'CurrentLbl4
+        '
+        Me.CurrentLbl4.AutoSize = True
+        Me.CurrentLbl4.Location = New System.Drawing.Point(198, 177)
+        Me.CurrentLbl4.Name = "CurrentLbl4"
+        Me.CurrentLbl4.Size = New System.Drawing.Size(23, 13)
+        Me.CurrentLbl4.TabIndex = 17
+        Me.CurrentLbl4.Text = "0 A"
+        '
+        'CurrentLbl5
+        '
+        Me.CurrentLbl5.AutoSize = True
+        Me.CurrentLbl5.Location = New System.Drawing.Point(198, 223)
+        Me.CurrentLbl5.Name = "CurrentLbl5"
+        Me.CurrentLbl5.Size = New System.Drawing.Size(23, 13)
+        Me.CurrentLbl5.TabIndex = 18
+        Me.CurrentLbl5.Text = "0 A"
+        '
+        'CurrentLbl6
+        '
+        Me.CurrentLbl6.AutoSize = True
+        Me.CurrentLbl6.Location = New System.Drawing.Point(198, 269)
+        Me.CurrentLbl6.Name = "CurrentLbl6"
+        Me.CurrentLbl6.Size = New System.Drawing.Size(23, 13)
+        Me.CurrentLbl6.TabIndex = 19
+        Me.CurrentLbl6.Text = "0 A"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(606, 324)
+        Me.Controls.Add(Me.CurrentLbl6)
+        Me.Controls.Add(Me.CurrentLbl5)
+        Me.Controls.Add(Me.CurrentLbl4)
+        Me.Controls.Add(Me.CurrentLbl3)
+        Me.Controls.Add(Me.CurrentLbl2)
+        Me.Controls.Add(Me.CurrentLbl1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TurnOff)
         Me.Controls.Add(Me.TurnOn)
         Me.Controls.Add(Me.Charger6)
@@ -105,9 +251,11 @@ Partial Class Form1
         Me.Controls.Add(Me.Charger3)
         Me.Controls.Add(Me.Charger2)
         Me.Controls.Add(Me.Charger1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
-        Me.Text = "Battery Charging Station Controls"
+        Me.Text = "FRC Team 1714 Battery Charging Station"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Charger1 As System.Windows.Forms.ProgressBar
@@ -118,5 +266,18 @@ Partial Class Form1
     Friend WithEvents Charger6 As System.Windows.Forms.ProgressBar
     Friend WithEvents TurnOn As System.Windows.Forms.Button
     Friend WithEvents TurnOff As System.Windows.Forms.Button
+    Friend WithEvents WaitTimer As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents CurrentLbl1 As System.Windows.Forms.Label
+    Friend WithEvents CurrentLbl2 As System.Windows.Forms.Label
+    Friend WithEvents CurrentLbl3 As System.Windows.Forms.Label
+    Friend WithEvents CurrentLbl4 As System.Windows.Forms.Label
+    Friend WithEvents CurrentLbl5 As System.Windows.Forms.Label
+    Friend WithEvents CurrentLbl6 As System.Windows.Forms.Label
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -2,11 +2,13 @@
 Imports Pololu.Usc
 
 Public Class Form1
-    Dim pololu As Usc
+    Dim controller As Usc 'Represents the Pololu controller
+    Dim inputs() As Pololu.Usc.ServoStatus 'Represents each port on the Pololu controller
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Connect to the Pololu Device
         Try
-            pololu = ConnectToDevice()
+            controller = ConnectToDevice()
         Catch exception As Exception
             MsgBox(exception.Message)
             Close()
@@ -22,22 +24,70 @@ Public Class Form1
             Return device 'Return the device
         Next
 
+        'If this function gets p
         Throw New Exception("Could not find Pololu device. Restart the application and try again")
     End Function
 
     Private Sub TurnOn_Click(sender As Object, e As EventArgs) Handles TurnOn.Click
-        'Turn on the relays
-        pololu.setTarget(0, 1500 * 4)
-        pololu.setTarget(2, 1500 * 4)
-        pololu.setTarget(4, 1500 * 4)
-        pololu.setTarget(6, 1500 * 4)
+        'Turn on the relays. You have to multiple the value by 4 for some reason
+        controller.setTarget(6, 1500 * 4)
+        controller.setTarget(7, 1500 * 4)
+        controller.setTarget(8, 1500 * 4)
+        controller.setTarget(23, 1500 * 4)
     End Sub
 
     Private Sub TurnOff_Click(sender As Object, e As EventArgs) Handles TurnOff.Click
         'Turn off the relays
-        pololu.setTarget(0, 0)
-        pololu.setTarget(2, 0)
-        pololu.setTarget(4, 0)
-        pololu.setTarget(6, 0)
+        controller.setTarget(6, 0)
+        controller.setTarget(7, 0)
+        controller.setTarget(8, 0)
+        controller.setTarget(23, 0)
+    End Sub
+    'Update the charger progress bars
+    Private Sub WaitTimer_Tick(sender As Object, e As EventArgs) Handles WaitTimer.Tick
+        'Variables that will hold the inputs from the chargers
+        Dim values(6) As Double
+        Dim percentCharge(6) As Double
+
+        'Calculate the voltage from the inputs
+        controller.getVariables(inputs)
+        For i As Integer = 0 To 5
+            'We divide the input by 201 to convert it to the actual voltage
+            'The value can theoretically never be less than 2.5
+            If (CDbl(inputs(i).position.ToString) / 201) < 2.55 Then
+                values(i) = 2.55
+            Else
+                values(i) = CDbl(inputs(i).position.ToString) / 201
+            End If
+        Next
+
+        'Calculate the current in amps and how charged the battery is in percent
+        For i As Integer = 0 To 6
+            'Change the range of the values from 0 to 2.55
+            Dim temp As Double = values(i) - 2.55
+            'Get the ratio of the temporary value to 2.55
+            Dim ratio As Double = temp / 2.55
+            'Subtract the ratio by 100 to get the percent done because if the ratio is 100
+            'that means the battery is fully uncharged.
+            percentCharge(i) = 1 - ratio
+            'Multiply that by 7.5 (the ammount of current it's drawing at 5.1 volts)
+            values(i) = ratio * 7.5
+        Next
+
+        'Set the values in the charger progress bars
+        Charger1.Value = CInt(percentCharge(0)) * 100
+        Charger2.Value = CInt(percentCharge(1)) * 100
+        Charger3.Value = CInt(percentCharge(2)) * 100
+        Charger4.Value = CInt(percentCharge(3)) * 100
+        Charger5.Value = CInt(percentCharge(4)) * 100
+        Charger6.Value = CInt(percentCharge(5)) * 100
+
+        'Calculate the amount of current that is being drawn (right now it's not current it's volts)
+        CurrentLbl1.Text = CStr(values(0)) + "A"
+        CurrentLbl2.Text = CStr(values(1)) + "A"
+        CurrentLbl3.Text = CStr(values(2)) + "A"
+        CurrentLbl4.Text = CStr(values(3)) + "A"
+        CurrentLbl5.Text = CStr(values(4)) + "A"
+        CurrentLbl6.Text = CStr(values(5)) + "A"
     End Sub
 End Class
